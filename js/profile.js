ef9303cbf1f537e9b6e504d800fe67ccbf0b34e9
firttrule <vanezik23@mail.ru>
2023-02-10 22:10:16 +0300

    Правки

--- js/profile.js
+++ js/profile.js
@@ -1,18 +1,19 @@
-window.onload = function() {
-    var arrowLeft = document.getElementById('arrowLeft')
-    var arrowRight = document.getElementById('arrowRight')
-    var achivLine = document.getElementById('achivLine')
-    var offset = 0;
+window.onload = function () {
+  var arrowLeft = document.getElementById("arrowLeft");
+  var arrowRight = document.getElementById("arrowRight");
+  var achivLine = document.getElementById("achivLine");
+  var offset = 0;
 
-    arrowLeft.onclick=function() {
-      offset = offset - 150;
-      achivLine.style.right = offset + 'px';
-
+  arrowLeft.onclick = function () {
+    if (offset > 0) {
+      offset = offset - 96;
+      achivLine.style.right = offset + "px";
     }
-    arrowRight.onclick=function() {
-      offset = offset + 150;
-      achivLine.style.right = offset + 'px';
+  };
+  arrowRight.onclick = function () {
+    if (offset <= 288) {
+      offset = offset + 96;
+      achivLine.style.right = offset + "px";
     }
-      
-
-}
+  };
+};
