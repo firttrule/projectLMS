cc030dad9b8d923234092d236dc6430216b7ad1f
firttrule <vanezik23@mail.ru>
2023-02-10 22:21:48 +0300

    Правки

--- js/about.js
+++ js/about.js
@@ -1,11 +1,13 @@
-window.onload = function() {
-    var coursesLine = document.getElementById('coursesLine')
-    var offset = 0;
-    function moveSLide() {
-        offset = offset + 289;
-        coursesLine.style.right =  offset + 'px';
-      }
-    setInterval(() => moveSLide(), 3000)
-      
-
-}
+window.onload = function () {
+  var coursesLine = document.getElementById("coursesLine");
+  var offset = 0;
+  function moveSLide() {
+    if (offset == 1280) {
+      offset = 0;
+    } else {
+      offset = offset + 320;
+    }
+    coursesLine.style.right = offset + "px";
+  }
+  setInterval(() => moveSLide(), 3000);
+};
